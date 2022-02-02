1645c7fe01d15736ad0f0a57361b9e0786a925cf
Bipul Chandro Roy <86865606+Sbipul@users.noreply.github.com>
2022-02-02 16:16:28 +0600

    Update Contact.js

--- src/pages/Home/Contact/Contact.js
+++ src/pages/Home/Contact/Contact.js
@@ -23,7 +23,7 @@ const Contact = () => {
                             <div className='d-flex text-start align-items-center'>
                                 <FcPhone className='bg-light p-2 rounded-circle' style={{fontSize : '3rem'}}/>
                                 <div className='ms-3'>
-                                    <h6 className='m-0'>Phone</h6>
+                                    <h6 className='m-0'>PhonE</h6>
                                     <p>+454552-54152 <br /> or, 59656-645564</p>
                                 </div>
                             </div>
